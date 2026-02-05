8e9533773d1fb82892a48af856cc540c056a0ade
Anastasiia Shapoval <nastya030207@gmail.com>
2026-02-05 21:08:19 +0200

    feat: updated Repository

--- src/main/java/ua/university/repository/Repository.java
+++ src/main/java/ua/university/repository/Repository.java
@@ -1,4 +1,9 @@
 package ua.university.repository;
 
-public class Repository {
+import java.util.List;
+
+public interface Repository<T> {
+
+    List<T> findAll();
+
 }
--- src/main/java/ua/university/repository/student/StudentRepository.java
+++ src/main/java/ua/university/repository/student/StudentRepository.java
@@ -1,4 +1,17 @@
 package ua.university.repository.student;
 
-public class StudentRepository {
+import ua.university.domain.Student;
+import ua.university.repository.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentRepository implements Repository<Student> {
+    private final List<Student> students = new ArrayList<>();
+
+    @Override
+    public List<Student> findAll() {
+        return List.copyOf(students);
+    }
+    // TODO: expand repository with save/delete methods in next stage
 }
