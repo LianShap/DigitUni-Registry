d66d92b4d56e7930b86569e36ca6e787d781d2a3
AnastasiaShkarovska <a.shkarovska@ukma.edu.ua>
2026-02-08 22:13:11 +0200

    Update toString and add equals in class Person.java

--- src/main/java/ua/university/domain/Person.java
+++ src/main/java/ua/university/domain/Person.java
@@ -1,11 +1,12 @@
 package ua.university.domain;
 import java.time.LocalDate;
+import java.util.Objects;
 
 public abstract class Person {
     private String id;
-    private String lastName;
-    private String firstName;
-    private String middleName;
+    private  String lastName;
+    private  String firstName;
+    private  String middleName;
     private LocalDate birthDate;
     private String email;
     private String phone;
@@ -20,15 +21,90 @@ public abstract class Person {
         this.email = email;
         this.phone = phone;
     }
+
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFullName(String fullName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getFullName() {
         return lastName + " " + firstName + " " + middleName;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) &&
+                Objects.equals(firstName, person.firstName) &&
+                Objects.equals(lastName, person.lastName) &&
+                Objects.equals(middleName, person.middleName) &&
+                Objects.equals(birthDate, person.birthDate) &&
+                Objects.equals(email, person.email) &&
+                Objects.equals(phone, person.phone);
+    }
+
     @Override
     public String toString() {
-        return getFullName() + ", email: " + email + ", phone: " + phone;
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
     }
 }
