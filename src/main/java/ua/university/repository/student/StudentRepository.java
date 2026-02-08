b0cc3d0ce177026919df67f8dcfb781fa0981f91
AnastasiaShkarovska <a.shkarovska@ukma.edu.ua>
2026-02-08 20:47:53 +0200

    Update StudentRepository.java

--- src/main/java/ua/university/repository/student/StudentRepository.java
+++ src/main/java/ua/university/repository/student/StudentRepository.java
@@ -1,7 +1,17 @@
 package ua.university.repository.student;
 
+public class StudentRepository {
 import ua.university.domain.Student;
 import ua.university.repository.Repository;
 
-public interface StudentRepository extends Repository<Student, String> {
-}
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentRepository implements Repository<Student> {
+    private final List<Student> students = new ArrayList<>();
+
+    @Override
+    public List<Student> findAll() {
+        return List.copyOf(students);
+    }
+    // TODO: expand repository with save/delete methods in next stage
