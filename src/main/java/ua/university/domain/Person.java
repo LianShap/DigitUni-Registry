4a97747e848f73633e388aabe5ab7cbd25ddc754
AnastasiaShkarovska <a.shkarovska@ukma.edu.ua>
2026-02-08 20:18:58 +0200

    Update Person.java

--- src/main/java/ua/university/domain/Person.java
+++ src/main/java/ua/university/domain/Person.java
@@ -1,9 +1,7 @@
 package ua.university.domain;
-
 import java.time.LocalDate;
 
 public abstract class Person {
-
     private String id;
     private String lastName;
     private String firstName;
@@ -26,39 +24,11 @@ public abstract class Person {
         return id;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getFullName() {
         return lastName + " " + firstName + " " + middleName;
+    }
+    @Override
+    public String toString() {
+        return getFullName() + ", email: " + email + ", phone: " + phone;
     }
 }
