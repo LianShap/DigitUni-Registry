9430588ada4380826cedfd926540b265206a39dc
AnastasiaShkarovska <a.shkarovska@ukma.edu.ua>
2026-02-08 20:19:56 +0200

    Update Department.java

--- src/main/java/ua/university/domain/Department.java
+++ src/main/java/ua/university/domain/Department.java
@@ -1,4 +1,6 @@
 package ua.university.domain;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Department {
 
@@ -7,6 +9,8 @@ public class Department {
     private Faculty faculty;
     private Teacher head;
     private String location;
+    private List<Student> students = new ArrayList<>();
+    private List<Teacher> teachers = new ArrayList<>();
 
     public Department(String code, String name, Faculty faculty,
                       Teacher head, String location) {
@@ -16,4 +20,18 @@ public class Department {
         this.head = head;
         this.location = location;
     }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    @Override
+    public String toString() {
+        return code + " - " + name + ", location: " + location;
+    }
+
 }
